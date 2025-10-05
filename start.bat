636f628f1391c4def7565c0f3b456746c24775df
Dev Brat Rai <dbrai890@gmail.com>
2025-10-05 14:26:25 +0530

    adding start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,17 @@
- 
+@echo off
+title Trading Monitor
+echo Starting Trading Monitor...
+
+REM Start backend
+cd backend
+start cmd /k "npm install && node server.js"
+
+REM Wait a few seconds for backend to start
+timeout /t 5
+
+REM Start frontend Angular app
+cd ../frontend
+start cmd /k "npm install && ng serve --open"
+
+exit
+REM End of script
